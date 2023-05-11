938eb696535942c80cfb8f42f82a82da95111bac
Viraj Gore <virajgore711@gmail.com>
2023-05-11 22:39:20 +0530

    Added nav link for add category

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 <body>
     
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="">Recipe Book</a>
+        <a class="navbar-brand" href="index.php">Recipe Book</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -18,6 +18,9 @@
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" href="add_recipe.php">Add Recipe</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="add_category.php">Add Category</a>
                 </li>
             </ul>
         </div>
