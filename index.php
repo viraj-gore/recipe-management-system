b991579e1da45221111a0cb22574afea49dec51c
Viraj Gore <virajgore711@gmail.com>
2023-05-11 22:41:11 +0530

    Modified index.php to display recipe list

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
-<?php include 'header.php'?>
-<div class="container">
-    <h1>Welcome to Recipe Management System</h1>
-    <p>This is a sample application for managing recipes, categories, and ingredients.</p>
+<?php
+    include 'header.php';
+    include 'credentials.php';
+// Establish database connection
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname,3308);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    // Retrieve all recipes
+    $sql = "SELECT * FROM recipe";
+    $result = mysqli_query($conn, $sql);
+
+    // Close database connection
+    mysqli_close($conn);
+?>
+<h1 class="text-center">Recipes</h1>
+<hr>
+<div class="list-group">
+    <?php while ($row = mysqli_fetch_assoc($result)): ?>
+        <a href="recipe.php?id=<?php echo $row['id']; ?>" class="list-group-item">
+            <?php echo $row['title'];?>
+        </a>
+    <?php endwhile;?>
 </div>
 <?php include 'footer.php'?>
