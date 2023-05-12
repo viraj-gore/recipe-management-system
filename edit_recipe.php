2e98924e542b25e96d0e7510ff33c98d19f1c1c1
Viraj Gore <virajgore711@gmail.com>
2023-05-12 10:50:26 +0530

    Added mysql function to get recipe details

--- edit_recipe.php
+++ edit_recipe.php
@@ -1,58 +1,54 @@
 <?php
 include 'header.php';
 // Establish database connection
-include 'credentials.php';
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname,3308);
-
+include 'db.php';
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
 // Retrieve recipe data for selected recipe
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $sql = "SELECT * FROM recipe WHERE id = $id";
-    $result = mysqli_query($conn, $sql);
-    $recipe = mysqli_fetch_assoc($result);
-}
-$categories = mysqli_query($conn, "SELECT * FROM category");
 $recipe_id = $_GET['id'];
-// Query ingredient data
-$sql = "SELECT i.name 
-        FROM ingredient i
-        JOIN recipe_ingredient ri ON i.ingredient_id = ri.ingredient_id
-        WHERE ri.recipe_id = $recipe_id";
+$sql = "SELECT get_recipe($recipe_id) AS recipe_data";
 $result = mysqli_query($conn, $sql);
-$ingredients = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$row = mysqli_fetch_assoc($result);
+$recipe_data = $row['recipe_data'];
+
+// Parse the recipe data into separate variables
+list($title,$category_id,$instructions, $ingredients) = explode('#', $recipe_data);
+// Retrieve all categories
+$sql = "SELECT * FROM category ORDER BY name ASC";
+$categories = mysqli_query($conn, $sql);
 // Close database connection
 mysqli_close($conn);
 ?>
 <h1 class="text-center">Edit Recipe</h1>
 <hr>
 <form action="save_recipe.php" method="post">
-    <input type="hidden" name="id" value="<?php echo $recipe['id']; ?>">
+    <input type="hidden" name="id" value="<?php echo $recipe_id; ?>">
     <div class="form-group">
         <label for="title">Title:</label>
-        <input type="text" class="form-control" id="title" name="title" value="<?php echo $recipe['title']; ?>" required>
+        <input type="text" class="form-control" id="title" name="title" value="<?php echo $title ?>" required>
     </div>
     <div class="form-group">
         <label for="category">Category:</label>
         <select class="form-control" id="category" name="category_id" required>
             <?php while ($row = mysqli_fetch_assoc($categories)): ?>
-                <option value="<?php echo $row['id']; ?>" <?php if ($recipe['category_id'] == $row['id']) { echo 'selected'; } ?>><?php echo $row['name']; ?></option>
+                <option value="<?php echo $row['id']; ?>" <?php if($category_id == $row['id']) { echo 'selected'; } ?>><?php echo $row['name']; ?></option>
             <?php endwhile; ?>
         </select>
     </div>
     <div class="form-group">
         <label for="instructions">Instructions:</label>
-        <textarea class="form-control" id="instructions" name="instructions" rows="5" required><?php echo $recipe['instructions']; ?></textarea>
+        <textarea class="form-control" id="instructions" name="instructions" rows="5" required><?php echo $instructions; ?></textarea>
     </div>
     <div class="form-group">
         <label for="ingredients">Ingredients:</label>
         <textarea class="form-control" id="ingredients" name="ingredients" rows="5" required>
-            <?php foreach ($ingredients as $ingredient): ?>
-                <?php echo ltrim($ingredient['name'],"")."\n"; ?>
+            <?php 
+                $ingredient_list = explode('|',$ingredients);
+                foreach($ingredient_list as $ingredient): ?>
+                    <?php echo ltrim($ingredient,"")."\n"; ?>
             <?php endforeach; ?>
         </textarea>
     </div>
