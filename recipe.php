9b7d47336980c1d7afd274c1b4840816a08fce44
Viraj Gore <virajgore711@gmail.com>
2023-05-12 10:51:14 +0530

    Added mysql function to retrieve the recipe

--- recipe.php
+++ recipe.php
@@ -1,9 +1,7 @@
 <?php
 include 'header.php';
 // Establish database connection
-include 'credentials.php';
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname,3308);
-
+include 'db.php';
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
@@ -11,32 +9,35 @@ if (!$conn) {
 
 // Get recipe ID from URL parameter
 $recipe_id = $_GET['id'];
-
-// Query recipe data
-$sql = "SELECT r.title, c.name as category_name, r.instructions 
-        FROM recipe r
-        JOIN category c ON r.category_id = c.id
-        WHERE r.id = $recipe_id";
+// Retrieve recipe data for selected recipe
+$recipe_id = $_GET['id'];
+$sql = "SELECT get_recipe($recipe_id) AS recipe_data";
 $result = mysqli_query($conn, $sql);
-$recipe = mysqli_fetch_assoc($result);
+$row = mysqli_fetch_assoc($result);
+$recipe_data = $row['recipe_data'];
 
-// Query ingredient data
-$sql = "SELECT i.name 
-        FROM ingredient i
-        JOIN recipe_ingredient ri ON i.ingredient_id = ri.ingredient_id
-        WHERE ri.recipe_id = $recipe_id";
-$result = mysqli_query($conn, $sql);
-$ingredients = mysqli_fetch_all($result, MYSQLI_ASSOC);
+// Parse the recipe data into separate variables
+list($title,$category_id,$instructions, $ingredients) = explode('#', $recipe_data);
+// Retrieve all categories
+$sql = "SELECT * FROM category ORDER BY name ASC";
+$categories = mysqli_query($conn, $sql);
 mysqli_close($conn); // Close database connection
 ?>
-<h1><?php echo $recipe['title']; ?></h1>
+<h1><?php echo $title; ?></h1>
+<p> 
+    <?php while ($row = mysqli_fetch_assoc($categories)): ?>
+        <?php if($category_id == $row['id']) { echo $row['name']; } ?>
+    <?php endwhile; ?>
+</p>
 <hr>
-<p><?php echo $recipe['instructions']; ?></p>
+<p><?php echo $instructions; ?></p>
 <hr>
 <h2>Ingredients</h2>
-<ul>
-    <?php foreach ($ingredients as $ingredient): ?>
-        <li><?php echo $ingredient['name']; ?></li>
+<ul style="margin-bottom: 8px;" class="list-group">
+    <?php 
+        $ingredient_list = explode('|',$ingredients);
+        foreach ($ingredient_list as $ingredient): ?>
+            <li class="list-group-item"><?php echo $ingredient; ?></li>
     <?php endforeach; ?>
 </ul>
 <a href="edit_recipe.php?id=<?php echo $recipe_id; ?> " class="btn btn-primary" > Edit Recipe </a>
